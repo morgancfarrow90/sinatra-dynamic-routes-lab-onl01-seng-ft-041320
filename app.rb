82b985c1b9bf33e17daeb93ff6a8b43eabe0d920
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-25 20:27:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,6 +29,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
     erb :operation
   end
 
