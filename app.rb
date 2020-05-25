97108ae79fd5dff34812c3f6fdd99eabd3c1589d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-25 20:16:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
-    @user_name = params[:name].to_s
+    @user_name = params[:name]
     erb :reversename
   end
 
